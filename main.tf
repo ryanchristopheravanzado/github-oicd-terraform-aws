3a958eee7b294b869fdce6c9444f017b13c63f7d
Ryan Christopher <ryanchristopher.avanzado@tup.edu.ph>
2023-02-02 14:35:07 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@ resource "aws_ssm_parameter" "Foo1" {
   type  = "String"
   value = "Barr1"
 }
+
+resource "aws_ssm_parameter" "Foo2" {
+  name  = "Foo2"
+  type  = "String"
+  value = "Barr2"
+}
+
+resource "aws_ssm_parameter" "Foo3" {
+  name  = "Foo3"
+  type  = "String"
+  value = "Barr3"
+}
