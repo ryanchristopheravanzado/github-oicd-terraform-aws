103a5fd7c24423d8eaacd6f4d6d2a6e02be4e8ea
Ryan Christopher <ryanchristopher.avanzado@tup.edu.ph>
2023-02-07 18:50:35 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
 resource "aws_ecr_repository" "x2con" {
   name = "x2con-dev"
 }
+
+locals {
+  image_tag = "latest"
+}
+
+data "docker_image" "example" {
+  name = "example_image:${local.image_tag}"
+}
+
+data "aws_ecr_login" "this" {}
+
+locals {
+  image_name = "${data.docker_image.example.name}"
+  image_tag = "${local.image_tag}"
+  repository_uri = "${aws_ecr_repository.example_repository.repository_url}"
+}
+
+resource "docker_image" "example" {
+  name = "${local.image_name}"
+  push = true
+
+  build {
+    context = "."
+  }
+
+  image_name = "${local.repository_uri}:${local.image_tag}"
+}
