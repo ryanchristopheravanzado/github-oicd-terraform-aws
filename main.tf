c556679781e521e05deaf8d2c3c70afd27c5cdf7
Ryan Christopher <ryanchristopher.avanzado@tup.edu.ph>
2023-02-08 10:30:58 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,28 @@ resource "aws_ecr_repository" "x2con-dev" {
   name = "x2con-dev"
 }
 
+data "aws_ecr_repository" "x2con-dev" {
+  name = aws_ecr_repository.x2con-dev.name
+}
+
 provider "aws" {
   region = "ap-southeast-1"
 }
 
 provider "docker" {
-  
+
+}
+
+locals {
+  repository_uri = data.aws_ecr_repository.x2con-dev.repository_uri
+}
+
+resource "null_resource" "example_image" {
+  provisioner "local-exec" {
+    command = <<EOT
+      docker build -t ${local.repository_uri}:latest .
+      $(aws ecr get-login --no-include-email --region us-west-2)
+      docker push ${local.repository_uri}:latest
+    EOT
+  }
 }
